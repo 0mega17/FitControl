b6784ebf8326a4e70d38d2f31b23c1b6ea6cae5d
0mega17 <sgaviria999@gmail.com>
2026-07-09 07:44:08 -0500

    feat: módulo de autenticación y usuarios

---
+++ backend/modules/users/users.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { getProfile, updateProfile, getAllUsers, updateUser, deactivateUser, activateUser, changeUserRole } = require('./user.controller');
+const { protect } = require('../../middleware/authMiddleware');
+const { authorize } = require('../../middleware/roleMiddleware');
+
+const router = express.Router();
+
+router.get('/profile', protect, getProfile);
+router.put('/profile', protect, updateProfile);
+router.get('/list', protect, authorize('Administrador'), getAllUsers);
+router.put('/:id', protect, authorize('Administrador'), updateUser);
+router.put('/:id/deactivate', protect, authorize('Administrador'), deactivateUser);
+router.put('/:id/activate', protect, authorize('Administrador'), activateUser);
+router.put('/:id/role', protect, authorize('Administrador'), changeUserRole);
+
+module.exports = router;
