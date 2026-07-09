fd065d47439a61c393f9400eb62566777d2bbfa5
0mega17 <sgaviria999@gmail.com>
2026-07-09 09:08:04 -0500

    refactor: normalizar nomenclatura a camelCase + español en feature/attendance

--- backend/modules/attendance/rutasAsistencia.js
+++ backend/modules/attendance/rutasAsistencia.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const { generateQR, registerByQR, registerManual, getByClient, getAll } = require('./attendance.controller');
-const { protect } = require('../../middleware/authMiddleware');
-const { authorize } = require('../../middleware/roleMiddleware');
+﻿const express = require('express');
+const { generateQR, registerByQR, registerManual, getByClient, getAll } = require('./controladorAsistencia');
+const { protect } = require('../../middleware/autenticacionMiddleware');
+const { authorize } = require('../../middleware/rolMiddleware');
 
 const router = express.Router();
 
